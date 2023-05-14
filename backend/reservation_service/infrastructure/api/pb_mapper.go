fc3783c210ceaad618d6df2593b23d9e8211c17d
TheWhiteRabbit21 <milutinovicn21@gmail.com>
2023-05-14 13:05:31 +0200

    Update pb_mapper.go

--- backend/reservation_service/infrastructure/api/pb_mapper.go
+++ backend/reservation_service/infrastructure/api/pb_mapper.go
@@ -1,1 +1,31 @@
 package api
+
+import (
+	"reservation_service/domain"
+
+	pb "github.com/OgnjenGolubovic/AirBnB/backend/common/proto/reservation_service"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func mapReservation(reservation *domain.AccommodationReservation) *pb.AccommodationReservation {
+	reservationPb := &pb.AccommodationReservation{
+		Id:              reservation.Id.Hex(),
+		AccommodationId: reservation.AccommodationId,
+		StartDate:       reservation.StartDate,
+		EndDate:         reservation.EndDate,
+	}
+
+	return reservationPb
+}
+
+func reverseMap(reservationPb *pb.AccommodationReservation) *domain.AccommodationReservation {
+	// id, _ := primitive.ObjectIDFromHex(accommodationPb.Id)
+	reservation := domain.AccommodationReservation{
+		Id:              primitive.NewObjectID(),
+		AccommodationId: reservationPb.AccommodationId,
+		StartDate:       reservationPb.StartDate,
+		EndDate:         reservationPb.EndDate,
+	}
+
+	return &reservation
+}
