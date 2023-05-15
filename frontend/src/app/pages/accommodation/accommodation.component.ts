8f8af180fa96011e8a775c85f1217ea39b3bfd41
TheWhiteRabbit21 <milutinovicn21@gmail.com>
2023-05-15 10:49:59 +0200

    update pretrage

--- frontend/src/app/pages/accommodation/accommodation.component.ts
+++ frontend/src/app/pages/accommodation/accommodation.component.ts
@@ -14,7 +14,7 @@ export class AccommodationComponent implements OnInit{
 
   public accommodations: Accommodation[] = [];
   
-  displayedColumns: string[] = ['id', 'name'];
+  displayedColumns: string[] = ['id', 'name', 'location', 'benefits', 'photos', 'minGuest', 'maxGuest'];
   dataSource = new MatTableDataSource(this.accommodations);
 
   constructor(public dialog: MatDialog, private _accommodationService: AccommodationService, private snackBar: MatSnackBar) {
@@ -44,11 +44,46 @@ export class AccommodationComponent implements OnInit{
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+  applyFilter2(event: Event) {
+
+    const filterValue = (event.target as HTMLInputElement).value;
+
+    this.dataSource.filterPredicate = (data, filter) => {
+      const row = data as Accommodation;
+      return this.filterRow2(row, filter);
+    };
+
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  applyFilter3(event: Event) {
+
+    const filterValue = (event.target as HTMLInputElement).value;
+
+    this.dataSource.filterPredicate = (data, filter) => {
+      const row = data as Accommodation;
+      return this.filterRow3(row, filter);
+    };
+
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+
+
+
   private filterRow(row: Accommodation, filterValue: string): boolean {
     // replace 'name' with the name of the column you want to filter
     return row.name.toLowerCase().includes(filterValue.toLowerCase());
   }
 
+  private filterRow2(row: Accommodation, filterValue: string): boolean {
+    // replace 'name' with the name of the column you want to filter
+    return row.location.toLowerCase().includes(filterValue.toLowerCase());
+  }
 
+  private filterRow3(row: Accommodation, filterValue: string): boolean {
+    // broj gostiju
+    return row.minGuest<= Number(filterValue) && row.maxGuest>= Number(filterValue);
+  }
 
 }
